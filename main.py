85dbe31ce5d8ff2b2b7c1733ce51202fc83e3147
Patrick Wöhrer <patrickwhrer@gmail.com>
2023-09-24 13:51:59 +0200

    python program now runs on pico - Amepl Programm

--- main.py
+++ main.py
@@ -1,10 +1,43 @@
-from machine
-from utime
+from machine import Pin, Timer
+import time
 
-ledPin = machine.Pin(25, machine.Pin.OUT)
+boardLed = Pin(25, Pin.OUT)
+blueLed = Pin(10, Pin.OUT)
+redLed = Pin(11, Pin.OUT)
+greenLed = Pin(12, Pin.OUT)
+count = 0
+i = 0
+j = range(9) #only odd numbers!
+
+def redOn():
+    blueLed.value(0)
+    redLed.value(1)
+    greenLed.value(0)
+    time.sleep(3)
+    
+def orangeOn():
+    blueLed.value(0)
+    redLed.value(1)
+    greenLed.value(1)
+    time.sleep(3)
+    
+def greenOn():
+    blueLed.value(0)
+    redLed.value(0)
+    greenLed.value(1)
+    time.sleep(3)
+
+    
+def greenBlink():
+    for i in j:
+        blueLed.value(0)
+        redLed.value(0)
+        greenLed.toggle()
+        time.sleep(0.25)
 
 while True:
-    ledPin.value(1)
-    utime.sleep(0.5)
-    ledPin.value(0)
-    utime.sleep(0.5)
+    redOn()
+    orangeOn()
+    greenOn()
+    greenBlink()
+    orangeOn()
